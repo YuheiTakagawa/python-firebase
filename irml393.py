0f7a9e981535057857c048f65f314a4025e19e66
YuheiTakagawa <roto11fon@gmail.com>
2019-05-03 19:55:11 +0900

    fix: change High -> Low for IR ML393

--- irml393.py
+++ irml393.py
@@ -1,13 +1,15 @@
 import setup_gpio
 import RPi.GPIO as GPIO
+from time import sleep
 
 list = [
-    {"pin":12, "io": GPIO.INPUT}
+    {"pin":12, "io": GPIO.IN}
 ]
 
 setup_gpio.setup(list)
 while True:
-    if GPIO.input(12) == GPIO.HIGH:
+    if GPIO.input(12) == GPIO.LOW:
         print("detected!!")
+    sleep(0.2)
 
         
