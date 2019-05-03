5c3617277be4e56b0504c85ca17a53fd4f171032
YuheiTakagawa <roto11fon@gmail.com>
2019-05-03 19:55:27 +0900

    fix: class

--- ad_convert.py
+++ ad_convert.py
@@ -3,16 +3,16 @@ import spidev
 import sys
 
 class SPI:
-    def __init__(no, num):
+    def __init__(self, no, num):
         this.spi = spidev.SpiDev()
         this.spi.open(no, num)
 
-    def convertVolts():
+    def convertVolts(self):
         v = (this.data * 3.3) / float(1023)
         self.volts = round(v, 4)
         return self.volts
 
-    def readAdc(channel):
+    def readAdc(self, channel):
         adc = this.spi.xfer2([0x01,0x80,0x00])
         self.data = ((adc[1]&3) << 8) | adc[2]
         print(str(adc[0] + "," + str(adc[1]) + "," + str(adc[2]))
