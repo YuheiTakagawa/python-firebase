f1f0256ae997d48e09af24c7871df71a486b42e3
YuheiTakagawa <roto11fon@gmail.com>
2019-05-04 00:46:37 +0900

    feat: add function return value

--- gps.py
+++ gps.py
@@ -10,10 +10,14 @@ class GPS:
     def rungps(self):
         for x in self.s.readline().decode('utf-8'):
             self.gps.update(x)
+    
+    def read(self):
+        rungps()
+        return (self.gps.latitude[0], self.gps.longitude[0])
 
 
 gps = GPS()
 while True:
-    gps.rungps()
-    print(str(gps.gps.latitude[0])+","+str(gps.gps.longitude[0]))
+    result = gps.read()
+    print(str(result)
     time.sleep(1)
