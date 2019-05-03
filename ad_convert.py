5b611f3832a762fe2453f3ff65920a501cb17e8a
YuheiTakagawa <roto11fon@gmail.com>
2019-05-03 17:28:05 +0900

    refactor: change class for ad converter

--- ad_convert.py
+++ ad_convert.py
@@ -2,24 +2,27 @@ import time
 import spidev
 import sys
 
-spi = spidev.SpiDev()
-spi.open(0,0)
+class SPI:
+    def __init__(no, num):
+        this.spi = spidev.SpiDev()
+        this.spi.open(no, num)
 
-def convertVolts(data):
-    volts = (data * 3.3) / float(1023)
-    volts = round(volts, 4)
-    return volts
+    def convertVolts():
+        v = (this.data * 3.3) / float(1023)
+        self.volts = round(v, 4)
+        return self.volts
 
-def readAdc(channel):
-    adc = spi.xfer2([0x01,0x80,0x00])
-    data = ((adc[1]&3) << 8) | adc[2]
-    print(str(adc[0] + "," + str(adc[1]) + "," + str(adc[2]))
-    return data
+    def readAdc(channel):
+        adc = this.spi.xfer2([0x01,0x80,0x00])
+        self.data = ((adc[1]&3) << 8) | adc[2]
+        print(str(adc[0] + "," + str(adc[1]) + "," + str(adc[2]))
+        return self.data
 
 while True:
-    data = readAdc(0)
+    spi = SPI()
+    data = spi.readAdc(0)
     print("adc: {:8}".format(data))
-    volts = convertVolts(data)
+    volts = spi.convertVolts()
     print("volts: {:8.2f}".format(volts))
 
     time.sleep(0.5)
